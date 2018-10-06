59fed8a25a06b2cef24b8753342799dd502e8092
h.mohamadi <hafez.gh.mohammadi@gmail.com>
2018-10-06 13:42:24 +0330

    .

---
+++ app/src/main/java/com/example/hmohamadi/ebooklibrary/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.hmohamadi.ebooklibrary;
+
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.os.Build;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.LinearLayout;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.example.hmohamadi.ebooklibrary.Helpers.ChangeActivityHelper;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private LinearLayoutCompat layoutMain;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Activity a = this;
+        YoYo.with(Techniques.FadeIn).duration(500).repeat(0).playOn(findViewById(R.id.layoutMain));
+        YoYo.with(Techniques.FadeIn).duration(1000).repeat(0).playOn(findViewById(R.id.txtSplashTtile));
+        YoYo.with(Techniques.FadeIn).duration(1500).repeat(0).onEnd(new YoYo.AnimatorCallback() {
+            @Override
+            public void call(Animator animator) {
+                ChangeActivityHelper.changeActivity(a,Main_Activity.class,true);
+            }
+        }).playOn(findViewById(R.id.imgSplash_logo));
+    }
+}
