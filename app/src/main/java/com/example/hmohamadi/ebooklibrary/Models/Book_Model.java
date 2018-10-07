0bc3ab9443c42981057a19ef6d5f4cb8517ef6ac
h.mohamadi <hafez.gh.mohammadi@gmail.com>
2018-10-07 20:08:39 +0330

    .

---
+++ app/src/main/java/com/example/hmohamadi/ebooklibrary/Models/Book_Model.java
@@ -0,0 +1,127 @@
+package com.example.hmohamadi.ebooklibrary.Models;
+
+import java.io.Serializable;
+
+public class Book_Model implements Serializable {
+    public Book_Model(int id, String name, int category_ID, String autorname, String year, int pagecount, String isbn, String url_path, String url_image, String keywords)
+    {
+        this.id = id;
+        this.name = name;
+        this.category_ID = category_ID;
+        this.autorname = autorname;
+        this.year = year;
+        this.pagecount = pagecount;
+        this.isbn = isbn;
+        this.url_path = url_path;
+        this.url_image = url_image;
+        this.keywords = keywords;
+    }
+
+    public Book_Model(int id, String name, String year, String autorname, String url_image) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.autorname = autorname;
+        this.url_image = url_image;
+    }
+
+    private int id;
+        private String name;
+        private int category_ID;
+        private String category_name;
+        private String autorname;
+        private String year;
+        private int pagecount;
+        private String isbn;
+        private String url_path;
+        private String url_image;
+        private String keywords;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategory_ID() {
+        return category_ID;
+    }
+
+    public void setCategory_ID(int category_ID) {
+        this.category_ID = category_ID;
+    }
+
+    public String getAutorname() {
+        return autorname;
+    }
+
+    public void setAutorname(String autorname) {
+        this.autorname = autorname;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getPagecount() {
+        return pagecount;
+    }
+
+    public void setPagecount(int pagecount) {
+        this.pagecount = pagecount;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getUrl_path() {
+        return url_path;
+    }
+
+    public void setUrl_path(String url_path) {
+        this.url_path = url_path;
+    }
+
+    public String getUrl_image() {
+        return url_image;
+    }
+
+    public void setUrl_image(String url_image) {
+        this.url_image = url_image;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+}
--- app/src/main/java/com/example/hmohamadi/ebooklibrary/SplashScreen.java
+++ app/src/main/java/com/example/hmohamadi/ebooklibrary/SplashScreen.java
@@ -5,10 +5,12 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -22,8 +24,8 @@ import com.example.hmohamadi.ebooklibrary.Helpers.ChangeActivityHelper;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private LinearLayoutCompat layoutMain;
 
+    AppCompatTextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,9 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         final Activity a = this;
+        //Typeface tf=  Typeface.createFromAsset(getAssets(),"fonts/b_davat.ttf");
+        //txt =  (AppCompatTextView) findViewById(R.id.txtSplashTtile);
+        //txt.setTypeface(tf);
         YoYo.with(Techniques.FadeIn).duration(500).repeat(0).playOn(findViewById(R.id.layoutMain));
         YoYo.with(Techniques.FadeIn).duration(1000).repeat(0).playOn(findViewById(R.id.txtSplashTtile));
         YoYo.with(Techniques.FadeIn).duration(1500).repeat(0).onEnd(new YoYo.AnimatorCallback() {
