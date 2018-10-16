ab0133fc549a19b17dbe8957d9c18db70e46821d
Mostafa Abdollahi <mostafa.abdollahi@yahoo.com>
2018-10-16 08:32:04 +0330

    AboutUs-ContactUs

---
+++ app/src/main/java/com/example/hmohamadi/ebooklibrary/WevViewActivity.java
@@ -0,0 +1,23 @@
+package com.example.hmohamadi.ebooklibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+import android.webkit.WebView;
+public class WevViewActivity extends AppCompatActivity {
+
+
+            private WebView webView;
+
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_wev_view);
+
+            webView = (WebView) findViewById(R.id.webView1);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl("http://www.saanei.xyz/?view=01,00,00,00,0");
+
+        }
+
+}
