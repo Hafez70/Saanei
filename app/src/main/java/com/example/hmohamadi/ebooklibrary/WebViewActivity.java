10c68728f470abdbb80e62e3d0f763c935dcb3fe
Mostafa Abdollahi <mostafa.abdollahi@yahoo.com>
2018-10-21 13:35:13 +0330

    WebView

--- app/src/main/java/com/example/hmohamadi/ebooklibrary/WebViewActivity.java
+++ app/src/main/java/com/example/hmohamadi/ebooklibrary/WebViewActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 public class WebViewActivity extends AppCompatActivity {
 
 
@@ -15,9 +18,27 @@ public class WebViewActivity extends AppCompatActivity {
             setContentView(R.layout.activity_wev_view);
 
             webView = (WebView) findViewById(R.id.webView1);
-            webView.getSettings().setJavaScriptEnabled(true);
+            WebSettings settings = webView.getSettings();
+            settings.setJavaScriptCanOpenWindowsAutomatically(true);
+            settings.setSupportMultipleWindows(true);
+            settings.setJavaScriptEnabled(true);
+            settings.setAppCacheEnabled(true);
+            settings.setDatabaseEnabled(true);
+            settings.setDomStorageEnabled(true);
+            settings.setGeolocationEnabled(true);
+            settings.setSaveFormData(false);
+            settings.setSavePassword(false);
+            settings.setSupportZoom(true);
+
             webView.loadUrl("http://www.saanei.xyz/?view=01,00,00,00,0");
 
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
         }
 
 }
