8ed405f2dd00b30fa1a39185b7eae291b7bea948
h.mohamadi <hafez.gh.mohammadi@gmail.com>
2018-10-23 10:52:53 +0330

    what the ffuck ?! 2

--- app/src/main/java/com/example/hmohamadi/ebooklibrary/ContactUsActivity.java
+++ app/src/main/java/com/example/hmohamadi/ebooklibrary/ContactUsActivity.java
@@ -1,41 +1,30 @@
 package com.example.hmohamadi.ebooklibrary;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.example.hmohamadi.ebooklibrary.Adapters.ExpandableRecyclerAdapter;
-import com.example.hmohamadi.ebooklibrary.Helpers.DividerItemDecoration;
-import com.example.hmohamadi.ebooklibrary.Models.ExpandableItemModel;
 
-import java.util.ArrayList;
-import java.util.List;
-import com.github.aakira.expandablelayout.Utils;
 
 
 public class ContactUsActivity extends AppCompatActivity {
+
+    public TextView txtTitle;
+    public TextView txtDescription;
+    public RelativeLayout buttonLayout;
+
+    public ConstraintLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactus);
 
-        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView_ContactUs);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this));
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        final List<ExpandableItemModel> data = new ArrayList<>();
-        data.add(new ExpandableItemModel(
-                getResources().getString(R.string.contactus_qom_title),
-                getResources().getString(R.string.contactus_qom_description) ,
-                Utils.createInterpolator(Utils.FAST_OUT_SLOW_IN_INTERPOLATOR)));
-        data.add(new ExpandableItemModel(
-                getResources().getString(R.string.contactus_tehran_title),
-                getResources().getString(R.string.contactus_tehran_description),
-                Utils.createInterpolator(Utils.FAST_OUT_SLOW_IN_INTERPOLATOR)));
 
-        recyclerView.setAdapter(new ExpandableRecyclerAdapter(data));
     }
 }
